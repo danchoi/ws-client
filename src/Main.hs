c97c3a8fdfcc88138373d362610a5fb0045f8136
DHC <danchoi@danielchoi.com>
2018-06-25 13:38:17 -0400

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 import qualified Network.WebSockets as WS
+import Data.Char (isAlpha)
 import Network.WebSockets 
 import Pipes
 import qualified Pipes.Prelude as P
@@ -13,10 +14,14 @@ import Data.Monoid
 import Control.Monad.Trans.Reader (ReaderT (runReaderT))
 import Options.Applicative
 import qualified Data.ByteString.Char8 as B8
+import Wuss
 
+data Protocol = Ws | Wss
+    deriving Show
 
 data Options = Options {
-    _host :: String
+    _protocol :: Protocol
+  , _host :: String
   , _port :: Int
   , _path :: String
   , _sessid :: B8.ByteString
@@ -24,10 +29,19 @@ data Options = Options {
 
 options :: Parser Options
 options = Options 
-  <$> strArgument (metavar "HOST")
+  <$> argument readProtocol (metavar "protocol")
+  <*> strArgument (metavar "HOST")
   <*> argument auto (metavar "PORT")
   <*> strArgument (metavar "PATH")
   <*> (B8.pack <$> strArgument (metavar "_sessid"))
+
+
+readProtocol :: ReadM Protocol
+readProtocol = eitherReader $ \s -> 
+    case Prelude.takeWhile (isAlpha) s of
+      "ws" -> Right Ws
+      "wss" -> Right Wss
+      x -> Left $ "Invalid protocol: " ++ x
 
 opts :: ParserInfo Options
 opts = info (options <**> helper)
@@ -36,22 +50,25 @@ opts = info (options <**> helper)
 main :: IO ()
 main = do
   Options{..} <- execParser opts
-  let hs = [("_sessid", _sessid)]
-  run _host _port _path hs
+  let hs = [("Cookie", "_sessid="<> _sessid)]
+  run _protocol _host _port _path [] -- hs
 
-run :: String  -- ^ host
+run :: Protocol
+    -> String  -- ^ host
     -> Int -- ^ port
     -> String -- ^ path
     -> Headers
     -> IO ()
-run host port path headers = do
+run proto host port path headers = do
 
     -- Setup a "mailbox" for getting messages from and placing them in.
     (output, input) <- spawn unbounded
     
     let 
 
-        msgsFrom  = runEffect $ wsIn >-> toOutput output
+        msgsFrom  = runEffect $ wsIn 
+                        >-> P.chain (\s -> lift $ putStrLn $ "Received 1: " ++ unpack s)
+                        >-> toOutput output
 
     lvf $ runEffect $ for (fromInput input)  
                           (\s -> lift $ putStrLn ("Received: " ++ unpack s))
@@ -59,6 +76,8 @@ run host port path headers = do
     let clientApp = \c -> lvf $ runReaderT msgsFrom c
 
 
-    WS.runClientWith host port path defaultConnectionOptions headers clientApp
+    case proto of
+      Wss -> Wuss.runSecureClientWith host (fromIntegral port) path defaultConnectionOptions headers clientApp
+      Ws -> WS.runClientWith host port path defaultConnectionOptions headers clientApp
   where
     lvf = liftIO . void . forkIO
